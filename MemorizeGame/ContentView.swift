6032cefae3c0ac17ed510267c5c31737dc53e83f
cozykb <weikun.l@qq.com>
2023-09-15 17:37:24 +0200

    Initial Commit

---
+++ MemorizeGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemorizeGame
+//
+//  Created by lwk on 2023/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by lwk on 2023/9/15.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
