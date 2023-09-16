b649ec20748183029798bb5519541a7660b2055a
cozykb <weikun.l@qq.com>
2023-09-16 20:01:24 +0200

    cardview isFront

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView(isFront: false)
+            CardView()
+            CardView()
         }
         .padding()
+        .foregroundColor(.orange)
     }
 }
+
+
+struct CardView: View{
+    var isFront: Bool = true
+    
+    var body: some View{
+        ZStack{
+            if isFront{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🤪")
+            }
+            else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
